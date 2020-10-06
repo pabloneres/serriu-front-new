aedbf549b3b05b8974c103d593e10418f2723863
Pablo Neres <pabloneres@hotmail.com>
2020-10-06 13:47:25 -0300

    Add bugfix

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,7 +1,11 @@
+import * as dotenv from 'dotenv';
 import axios from 'axios'
+dotenv.config()
+
+
 
 const baseUrl = axios.create({
-  baseURL: 'https://serriu-back.herokuapp.com'
+  baseURL: process.env.APP_URL
 })
 
 export default baseUrl
