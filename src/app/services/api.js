a11785e1d7bc85b0c3cf309aa4f744afaaa1d80e
Pablo Neres <pabloneres@hotmail.com>
2020-10-29 18:58:48 -0300

    teste nova api

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseUrl = axios.create({
   // baseURL: 'http://serriu-back.herokuapp.com'
   // baseURL: 'http://127.0.0.1:3333'
-  baseURL: 'http://157.230.214.88'
+  baseURL: 'https://api.serriu.com.br'
 })
 
-export default baseUrl
+export default baseUrl  
