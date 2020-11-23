61524e80fd10a8193835c1413742c9112b9be50a
Pablo Neres <48608017+pabloneres@users.noreply.github.com>
2020-11-23 19:18:31 -0300

    Update api.js

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const baseUrl = axios.create({
   //baseURL: 'http://serriu-back.herokuapp.com'
-  baseURL: 'http://127.0.0.1:3333'
+  // baseURL: 'http://127.0.0.1:3333'
   // baseURL : 'http://26.206.241.138:3333'
-  // baseURL: 'https://api.serriu.com.br'
+  baseURL: 'https://api.serriu.com.br'
 })
 
 export default baseUrl  
