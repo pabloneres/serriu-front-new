d9397d1b683ff0a29dece07c33943eacf4cdf0aa
Pablo Neres <pabloneres@hotmail.com>
2021-02-15 21:13:44 -0300

    new update

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const baseUrl = axios.create({
   //baseURL: 'http://serriu-back.herokuapp.com'
   // baseURL: "http://127.0.0.1:3333"
-  // baseURL : 'http://26.206.241.138:3333'
-  baseURL: "http://api.serriu.com.br"
+  baseURL : 'http://164.90.130.146'
+  // baseURL: "http://api.serriu.com.br"
 });
 
 export default baseUrl;
